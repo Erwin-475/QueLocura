b469c2cf0d06c02c6e78aa2ab703168c2d36b5d1
Erwin-475 <erwinyluismario@gmail.com>
2025-07-24 05:52:45 -0500

    b

--- Frontend/src/api/products.js
+++ Frontend/src/api/products.js
@@ -2,7 +2,7 @@ console.log("API URL:", import.meta.env.VITE_API_URL)
 
 const getProducts = async () => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/getAll/products`)
+        const response = await fetch(`${import.meta.env.VITE_API_URL}`)
         return await response.json()
     } catch (error) {
         console.error('Error fetching products:', error)
